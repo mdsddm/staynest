254e415e0e57782ffb0d3ea35c7e625650c9dc67
mdsddm <md.saddam.gtm@gmail.com>
2026-01-21 14:02:27 +0530

    added file upload

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,9 +4,11 @@ module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
 };
+
 module.exports.renderNewForm = async (req, res) => {
   res.render("listings/new.ejs");
 };
+
 module.exports.showListing = async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id)
@@ -23,13 +25,18 @@ module.exports.showListing = async (req, res) => {
   }
   res.render("listings/show.ejs", { listing });
 };
+
 module.exports.createListing = async (req, res, next) => {
+  let url = req.file.path;
+  let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
+  newListing.image = {filename,url};
   await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
+
 module.exports.renderEditForm = async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
@@ -39,12 +46,14 @@ module.exports.renderEditForm = async (req, res) => {
   }
   res.render("listings/edit.ejs", { listing });
 };
+
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   req.flash("success", "Listing Updating!");
   res.redirect(`/listings/${id}`);
 };
+
 module.exports.destroyListing = async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndDelete(id);
--- routes/listing.js
+++ routes/listing.js
@@ -3,6 +3,9 @@ const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 // Index Route
 router
@@ -11,6 +14,7 @@ router
   .post(
     isLoggedIn,
     validateListing,
+    upload.single('listing[image]'),
     wrapAsync(listingController.createListing),
   );
 
